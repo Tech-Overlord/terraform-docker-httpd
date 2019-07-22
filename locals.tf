1709a7d4301ad508fd61d6487eb1689c5209a156
amuhammad <amuhammad@ctacorp.com>
2019-07-22 14:49:08 -0400

    upgraded to terraform 0.12.5

--- locals.tf
+++ locals.tf
@@ -1,6 +1,7 @@
 locals {
-  image_name = "httpd:latest"
+  image_name     = "httpd:latest"
   container_name = "myhttpdserver"
-  int_port = "80"
-  ext_port = "80"
+  int_port       = "80"
+  ext_port       = "80"
 }
+
--- main.tf
+++ main.tf
@@ -1,15 +1,13 @@
-resource "docker_image" "httpd-image-resource"
-{
-  name = "${local.image_name}"
+resource "docker_image" "httpd-image-resource" {
+  name = local.image_name
 }
 
-resource "docker_container" "httpd-container-resource"
-{
-  name = "${local.container_name}"
-  image = "${local.image_name}"
-  ports
-  {
-    internal = "${local.int_port}"
-    external = "${local.ext_port}"
+resource "docker_container" "httpd-container-resource" {
+  name  = local.container_name
+  image = local.image_name
+  ports {
+    internal = local.int_port
+    external = local.ext_port
   }
 }
+
